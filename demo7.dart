c6ac81e74ab916e16e505ed16f69fd3cb7a1cc9e
Shiva Charan <89567151+charan00321@users.noreply.github.com>
2025-03-12 06:45:10 +0530

    demo7.dart

---
+++ demo7.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(){
+
+int a = 5;
+int b = 4;
+int c = 6;
+
+if (a>b && a>c){
+	
+	print('a is greater ');
+}else{
+	if(b>a && b>c)
+
+	print('b is greater ');
+
+else{
+	print('c is greater');
+
+}
+}
+
+}
